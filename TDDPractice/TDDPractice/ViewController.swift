a497c953d61db33acaeca58520314071a5eab40e
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-01-24 19:47:25 +0900

    init project
    start project

---
+++ TDDPractice/TDDPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TDDPractice
+//
+//  Created by 김윤석 on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
