a4f0980a40d01f141583e96a5f2f7ed108a71266
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-01-25 19:31:03 +0900

    add decodable model

---
+++ TDDPractice/TDDPractice/SignUp/Model/SignupResponseModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignupResponseModel.swift
+//  TDDPractice
+//
+//  Created by 김윤석 on 2022/01/24.
+//
+
+import Foundation
+
+struct SignupResponseModel: Decodable {
+    let status: String
+    
+}
