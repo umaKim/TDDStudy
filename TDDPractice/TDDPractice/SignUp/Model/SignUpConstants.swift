b006d25f23ba278e140feaf8b3dc89920f731857
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2022-01-25 19:28:44 +0900

    add model

---
+++ TDDPractice/TDDPractice/SignUp/Model/SignUpConstants.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpConstants.swift
+//  TDDPractice
+//
+//  Created by 김윤석 on 2022/01/24.
+//
+
+import Foundation
+
+struct SignupConstants {
+    static let firstNameMinLength = 2
+    static let firstNameMaxLength = 10
+    static let lastNameMinLength = 2
+    static let lastNameMaxLength = 12
+    static let passwordMinLength = 8
+    static let passwordMaxLength = 16
+    static let signupURLString = ""
+}
